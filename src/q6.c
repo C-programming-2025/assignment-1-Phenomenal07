7530f47de542fb27645af9674df3a1bd9af78b76
Phenomenal07 <maharjansaugat07@gmail.com>
2025-08-25 11:17:22 +0545

    Update q6.c

--- src/q6.c
+++ src/q6.c
@@ -1,12 +1,12 @@
 // Write a C program to calculate the area of a rectangle. Prompt the user to enter the length and width, and display the result.
 #include <stdio.h>
 int main() {
-    float length, width, area;
+    float l, w, a;
     printf("Enter the length of the rectangle: ");
-    scanf("%f", &length);
+    scanf("%f", &l);
     printf("Enter the width of the rectangle: ");
-    scanf("%f", &width);
-    area = length * width;
-    printf("The area of the rectangle is: %.2f\n", area);
+    scanf("%f", &w);
+    a = l * w;
+    printf("The area of the rectangle is: %f\n", a);
     return 0;
 }
