e82d630ca09437774d1f497f197b780c91adc570
Phenomenal07 <maharjansaugat07@gmail.com>
2025-08-28 10:34:20 +0545

    Assignment Updates

--- src/q3.c
+++ src/q3.c
@@ -2,7 +2,7 @@
 int main() {
     char name[20];
     printf("Enter your name: ");
-    scanf(name, "%s");
-    printf("Hello, %s! Welcome to C programming!\n", name);
+    scanf("%s", name);
+    printf("Hello! %s Welcome to C programming!\n", name);
     return 0;
 }
