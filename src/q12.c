53868edae27001f0c9bd131ac3f0960b1cade904
Phenomenal07 <maharjansaugat07@gmail.com>
2025-08-28 09:57:09 +0545

    Assignment Update

--- src/q12.c
+++ src/q12.c
@@ -1,1 +1,10 @@
 // Given three variables a, b, and c, write an expression that checks if a is greater than b and c is not equal to 0.
+#include <stdio.h>
+int main() {
+    int a, b, c, result;
+    printf("Enter three integers (a, b, c): ");
+    scanf("%d %d %d", &a, &b, &c);
+    result = (a > b) && (b > c) && (c != 0);
+    printf("Result: %d\n", result);
+    return 0;
+}
