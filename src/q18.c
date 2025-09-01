9e98891a62dcf678fece258d9b2f192c1fff5d42
Phenomenal07 <maharjansaugat07@gmail.com>
2025-09-01 10:09:55 +0545

    update

--- src/q18.c
+++ src/q18.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main() {
     int a, b, c;
     int sum;
     printf("enter three different numbers:");
-    scanf("%d %d %d", &a,&b,&c); 
+    scanf("%d %d %d", &a, &b, &c); 
     sum = (a * a) + (b * b) + (c * c);
     printf("sum of squares=%d", sum);
     return 0;
