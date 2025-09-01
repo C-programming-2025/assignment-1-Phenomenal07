279f8961aef7723896e9122a718d196cbc86c0f3
Phenomenal07 <maharjansaugat07@gmail.com>
2025-09-01 09:58:20 +0545

    update

--- src/q12.c
+++ src/q12.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main(){
-    int a, b, c, result1, result2;
+    int a,b, c, result1, result2;
     printf("enter a, b, c:");
     scanf("%d %d %d", &a,&b,&c); 
-    result1 = (a > b) && (a > c);
-    printf("%d",result1); // if prints 1 then true a greater than b and c
+    result1 = (a>b)&& (a>c);
+    printf("%d",result1); 
     result2= (c!=0);
-    printf("\n%d", result2); // if prints 1 then true c is not 0 
-    return 0;
+    printf("\n%d",result2);
 }
