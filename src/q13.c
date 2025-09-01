5804f600bb9e7662bc7cbc95341eaf3c04371dca
Phenomenal07 <maharjansaugat07@gmail.com>
2025-09-01 10:06:51 +0545

    update

--- src/q13.c
+++ src/q13.c
@@ -4,6 +4,6 @@ int main(){
     printf("enter a number:");
     scanf("%d", &num); 
     result = ((num / 2) * 2 == num) && ((num / 3) * 3 == num);
-    printf("%d", result); // if prints 1 then true num is divisible by both 2 and 3
+    printf("%d", result);
     return 0;
 }
